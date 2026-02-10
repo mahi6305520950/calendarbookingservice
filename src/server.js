b6abe5a083b7752a1bdee43129cef53ce63c3fc3
mahi6305520950 <148845470+mahi6305520950@users.noreply.github.com>
2026-02-10 11:03:32 +0530

    Change server listen address to '0.0.0.0'

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
     await sequelize.sync();
     console.log("Database synced");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
